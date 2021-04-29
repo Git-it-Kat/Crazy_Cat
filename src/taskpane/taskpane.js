8b15fae1abc12aefc2b04a5b47dcc47e71872505
laughatpain <laughatpain@users.noreply.github.com>
2021-04-29 11:47:43 -0600

    addCloseEvent added to taskpane.js

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -49,3 +49,16 @@ function addSign(){
   addCloseEvent();
 
 }
+//function add closeevent
+function addCloseEvent(){
+  // Click on a close button to hide the current list item
+  var i;
+  var close = document.getElementsByClassName("close");
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+    }
+  }
+}
